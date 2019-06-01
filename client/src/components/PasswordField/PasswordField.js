8ef489e25b848d59c7bded33a981cb894982cab1
reenan <0renan.souza@gmail.com>
2019-06-01 19:17:28 -0300

    add pass ocmponent

---
+++ client/src/components/PasswordField/PasswordField.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import PasswordMask from 'react-password-mask';
+import './PasswordField.scss';
+
+export default class PasswordField extends Component {
+
+	static propTypes = {
+		onChange: PropTypes.func.isRequired,
+		value: PropTypes.string.isRequired,
+	}
+
+	render() {
+		const { value, onChange } = this.props;
+
+		return (
+			<div className='password-wrapper'>
+					<label>
+						<span>Password:</span>
+						<PasswordMask
+							value={value}
+							maxLength={50}
+							onChange={onChange}
+							inputClassName='input-password'
+							buttonClassName='btn-show-hide'
+							useVendorStyles={false}
+						/>
+					</label>
+				</div>
+		);
+	}
+}
