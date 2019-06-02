343267d2c469d3f585d28dac4e8d8a7d19db866d
reenan <0renan.souza@gmail.com>
2019-06-02 16:09:48 -0300

    few more tests

--- client/src/pages/Upload/Upload.test.js
+++ client/src/pages/Upload/Upload.test.js
@@ -2,8 +2,111 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Upload from './Upload';
 
+import { shallow } from 'enzyme'; 
+
+import renderer from 'react-test-renderer';
+import sinon from 'sinon';
+
+
+afterEach(() => {
+  // Restore the default sandbox here
+  sinon.restore();
+});
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Upload />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('update file state correctly with null', () => {
+  const component = renderer.create(<Upload />);
+  const instance = component.getInstance();
+
+  const mockEvent = {
+    target: {
+      files: [null]
+    }
+  }
+
+  instance.onMediaChange(mockEvent);
+
+  expect(instance.state.file).toBe(null)
+});
+
+it('update file state correctly with mock', () => {
+  const onload = () => {};
+
+  sinon.stub(window, 'FileReader').returns({
+    onload: onload,
+    readAsDataURL: (file) => {
+      onload(file)
+    }
+  });
+
+  const component = renderer.create(<Upload />);
+  const instance = component.getInstance();
+
+  const mockFile = {target: { result: 'mock' }};
+  const mockEvent = {
+    target: {
+      files: [mockFile]
+    }
+  }
+
+  instance.onMediaChange(mockEvent);
+
+  expect(instance.state.file).toBe(mockFile)
+});
+
+it('update file state correctly via define file (video)', () => {
+  const onload = () => {};
+
+  sinon.stub(window, 'FileReader').returns({
+    onload: onload,
+    readAsDataURL: (file) => {
+      onload(file)
+    }
+  });
+
+  const component = shallow(<Upload />);
+  const instance = component.instance();
+
+  const mockFile = { type: 'video', target: { result: 'mock' }};
+  const mockEvent = {
+    target: {
+      files: [mockFile]
+    }
+  }
+
+  instance.onMediaChange(mockEvent);
+  instance.defineFile(mockFile);
+
+  expect(instance.state.file.content).toBe('mock')
+});
+
+it('update file state correctly via define file (gif)', () => {
+  const onload = () => {};
+  
+  sinon.stub(window, 'FileReader').returns({
+    onload: onload,
+    readAsDataURL: (file) => {
+      onload(file)
+    }
+  });
+
+  const component = shallow(<Upload />);
+  const instance = component.instance();
+
+  const mockFile = { type: 'gif', target: { result: 'mock' }};
+  const mockEvent = {
+    target: {
+      files: [mockFile]
+    }
+  }
+
+  instance.onMediaChange(mockEvent);
+  instance.defineFile(mockFile);
+
+  expect(instance.state.file.content).toBe('mock')
 });
--- client/src/pages/Upload/components/GIFWrapper/GIFWrapper.test.js
+++ client/src/pages/Upload/components/GIFWrapper/GIFWrapper.test.js
@@ -3,14 +3,40 @@ import ReactDOM from 'react-dom';
 import GIFWrapper from './GIFWrapper';
 import { MemoryRouter } from 'react-router-dom';
 
+import { shallow } from 'enzyme'; 
+
+import renderer from 'react-test-renderer';
+import sinon from 'sinon';
+
+
+afterEach(() => {
+  // Restore the default sandbox here
+  sinon.restore();
+});
+
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
 
-  const GIF = new File(['bigbase64'], 'mock.gif', { type: 'image/gif' })
-
   ReactDOM.render(
     <MemoryRouter>
-      <GIFWrapper GIF={GIF} />
+      <GIFWrapper GIF={{ content: 'mock' }} />
     </MemoryRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('should handle upload correcly', async () => {
+  sinon.stub(window, 'fetch').resolves({
+    json: () => { return {
+      id: 'mock'
+    }}
+  });
+
+  const component = shallow(<GIFWrapper GIF={{ content: 'mock' }} />);
+  const instance = component.instance();
+
+  const returnedID = await instance.uploadGIF(false, null, null);
+
+  expect(returnedID).toBe('mock');
+
+})
