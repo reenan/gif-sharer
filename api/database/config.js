8aaffd586f9c0cda6ede61b5bc00e34ed2d07a31
reenan <0renan.souza@gmail.com>
2019-06-01 21:06:03 -0300

    adjust env var

--- api/database/config.js
+++ api/database/config.js
@@ -2,7 +2,7 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: 'gif-sharer',
-  host: process.env.CLEARDB_DATABASE_URL,
+  host: process.env.DB_HOST,
   dialect: 'mysql',
   define: {
     freezeTableName: true,
